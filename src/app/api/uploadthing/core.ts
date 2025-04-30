d2a9604d510f5122d75c556704f82946405e9cd9
Luis Gomez <lgomez00714@gmail.com>
2025-04-29 18:33:45 -0700

    Added live chat functionality

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,5 +1,6 @@
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
+import streamServerClient from "@/lib/stream";
 import { createUploadthing, FileRouter } from "uploadthing/next";
 import { UploadThingError, UTApi } from "uploadthing/server";
 
@@ -33,12 +34,21 @@ export const fileRouter = {
 
       const newAvatarUrl = file.ufsUrl
 
-      await prisma.user.update({
-        where: { id: metadata.user.id },
-        data: {
-          avatarUrl: newAvatarUrl,
-        },
-      });
+      await Promise.all([
+        prisma.user.update({
+          where: {id: metadata.user.id},
+          data: {
+            avatarUrl: newAvatarUrl
+          }
+        }),
+        streamServerClient.upsertUser({
+          id: metadata.user.id,
+          set: {
+            image: newAvatarUrl,
+          }
+        })
+      ])
+
 
       return { avatarUrl: newAvatarUrl };
     }),
